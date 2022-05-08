386114f7828dc1fc025c0bc857c0896f9bffdaed
Agata Żywot <agata.zywot@gmail.com>
2022-05-08 20:57:04 +0200

    Json transformer 1 class

--- src/main/java/pl/put/poznan/json_tool/logic/JsonTransformer.java
+++ src/main/java/pl/put/poznan/json_tool/logic/JsonTransformer.java
@@ -2,21 +2,30 @@ package pl.put.poznan.json_tool.logic;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 
 public class JsonTransformer {
+
+    private ObjectMapper mapper;
     private JsonNode node;
 
-    public JsonTransformer(String stringJson) throws JsonProcessingException {
-        JsonReader transformer = new JsonReader(stringJson);
-        this.node = transformer.getJsonNode();
+    public JsonTransformer(String source) throws JsonProcessingException {
+        this.mapper = getObjectMapper();
+        this.node = mapper.readTree(source);
     }
 
-    public String minifyJson(){
-        return "";
+    private static ObjectMapper getObjectMapper(){
+        ObjectMapper defaultObjectMapper = new ObjectMapper();
+        return defaultObjectMapper;
     }
 
-    public String unminifyJson(){
-        return "";
+    public String minify() throws JsonProcessingException {
+        return mapper.writeValueAsString(node);
+    }
+
+    public String unminify() throws JsonProcessingException {
+        return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(node);
     }
 
     public String removeKeys(String[] keys){
---
+++ src/test/java/pl/put/poznan/json_tool/logic/JsonTransformerTest.java
@@ -0,0 +1,30 @@
+package pl.put.poznan.json_tool.logic;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JsonTransformerTest {
+
+    String simpleJson = "{ \"id\": \"0001\",\n" +
+            "\t\"type\": \"donut\",\n" +
+            "\t\"name\": \"Cake\",\n" +
+            "\t\"ppu\": 0.55 }";
+
+    @Test
+    void minify() throws JsonProcessingException {
+        JsonTransformer transformer = new JsonTransformer(simpleJson);
+        System.out.println("MINIFIED:");
+        System.out.println(transformer.minify());
+    }
+
+    @Test
+    void unminify() throws JsonProcessingException {
+        JsonTransformer transformer = new JsonTransformer(simpleJson);
+        System.out.println("\nUNMINIFIED:");
+        System.out.println(transformer.unminify());
+
+    }
+}
