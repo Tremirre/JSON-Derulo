c6e152871a2891bdbce2c3e19bc338b83d028a77
AndrzejKaj <andrzej.kajdasz1@gmail.com>
2022-06-08 14:44:16 +0200

    Add More Test

--- src/test/java/pl/put/poznan/json_tool/logic/textparsing/TextFinderTest.java
+++ src/test/java/pl/put/poznan/json_tool/logic/textparsing/TextFinderTest.java
@@ -1,10 +1,12 @@
 package pl.put.poznan.json_tool.logic.textparsing;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class TextFinderTest {
     String simpleJson = "{ \"id\": \"0001\",\n" +
@@ -18,5 +20,17 @@ public class TextFinderTest {
     void differentLines(){
         TextFinder finder = new TextFinder(simpleJson, text);
         assertEquals(finder.findLines(), Arrays.asList(2));
+    }
+
+    @Test
+    void emptyKeyTest(){
+        TextFinder finder = new TextFinder(simpleJson, "");
+        assertEquals(finder.findLines(), Arrays.asList(1,2,3,4));
+    }
+
+    @Test
+    void wrongKeyTest(){
+        TextFinder finder = new TextFinder(simpleJson, "test");
+        assertEquals(finder.findLines(), Arrays.asList());
     }
 }
